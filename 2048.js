3ae3562c3d09c934d06ab271302843a11503e92b
gominnam <kosok03@naver.com>
2021-09-06 13:31:40 +0900

    2048GAME

---
+++ 2048.js
@@ -0,0 +1,17 @@
+class Game2048{
+    constructor(className){
+        
+        $(className).find("td").css({
+            "border": "3px solid #DRDRDR",
+            "height": "100px",
+            "width": "100px",
+            "text-align": "center",
+            "font-size": "30px",
+            "font-weight": "bold",
+            "color": "#F49551",
+            "background-color": "#BEBEBE"
+        })
+
+        
+    }
+}
