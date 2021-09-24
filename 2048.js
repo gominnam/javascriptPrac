095648c6243cd53d489c713562c34ba49b76ec7f
gominnam <kosok03@naver.com>
2021-09-24 20:38:37 +0900

    2048 js

--- 2048.js
+++ 2048.js
@@ -14,15 +14,16 @@ class Game2048{
     }
 }
 var score = 0;
+var board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
 
 init();
 
 //초기화 화면
 function init(){
-    for(var i=1; i<=4; i++){
-        for(var j=1; j<=4; j++){
-            var boardId = i*10 + j + "";
-            $('#' + boardId).text(0);
+    for(var i=0; i<4; i++){
+        for(var j=0; j<4; j++){
+            var boardId = i + "" + j;
+            $('#' + boardId).text(board[i][j]);
         }
     }
 
@@ -32,16 +33,28 @@ function init(){
 }
 
 function createNumber(){
+    for(var i=0; i<4; i++){
+        for(var j=0; j<4; j++){
+            var boardId = i + "" + j;
+            //$('#' + boardId).text(0);
+            $('#' + boardId).text(board[i][j]);
+        }
+    }
+
     var check = true;
     while(check){
-        var x = parseInt(Math.random()*4 + 1) * 10;
-        var y = parseInt(Math.random()*4 + 1);
-        var pos = x + y;
+        var x = parseInt(Math.random()*4);
+        var y = parseInt(Math.random()*4);
+        var pos = x + "" + y;
+        console.log(pos);
         if($('#' + pos).text() == 0){
             $('#' + pos).text(2);
+            board[y][x] = 2;
             check = false;
         }
     }
+    $('#score').html("SCORE : " + score);
+    //document.getElementById("score").innerHTML=score;
 }
 
 //키보드 입력 이벤트
@@ -60,20 +73,88 @@ $(document).ready(function() {
 function keyDir(e){
     switch(e){
         case "up":
-            rotation(0);
+            calculation();
+            createNumber();
             break;
         
         case "down":
+            rotation(2);
+            calculation();
+            rotation(2);
+            createNumber();
             break;
 
         case "left":
+            rotation(1);
+            calculation();
+            rotation(3);
+            createNumber();
             break;
         
         case "right":
+            rotation(3);
+            calculation();
+            rotation(1);
+            createNumber();
             break;
     }
 }
 
-function rotation(){
-    
+function rotation(r){
+    for(var k=0; k<r; k++){
+        var tmp = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+        for(var i=0; i<4; i++){
+            for(var j=0; j<4; j++){
+                tmp[i][j] = board[i][j];
+            }
+        }
+        console.log("before");
+        console.log(board);
+        for(var i=0; i<4; i++){
+            for(var j=0; j<4; j++){
+                board[j][3-i] = tmp[i][j];
+            }
+        }
+        console.log("after");
+        console.log(board);
+    }
+}
+
+function calculation(){
+    var flag = false;
+    var tmp = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    console.log(board);
+    for(var i=0; i<4; i++){
+        var rowArr = [board[0][i], board[1][i], board[2][i], board[3][i]];
+        console.log(rowArr);
+        for(var j=0; j<3; j++){//덧셈 2 0 0 2 일 때 계산 안되는 문제 해결하기
+            if(rowArr[j] == 0) continue;
+            else if(rowArr[j] == rowArr[j+1]){
+                rowArr[j] += rowArr[j+1];
+                rowArr[j+1] = 0;
+                score += rowArr[j];
+                flag = true;
+            }
+        }
+        var pos = 0;
+        for(var j=0; j<4; j++){
+            if(rowArr[j] != 0){
+                //tmp[i][pos++] = rowArr[j];
+                tmp[pos++][i] = rowArr[j];
+            }
+            else flag = true;
+        }
+    }
+
+    board = tmp;
+    if(!flag) {
+        var maxNum = 0;
+        for(var i=0; i<4; i++){
+            for(var j=0; j<4; j++){
+                if(board[i][j] > maxNum) maxNum = board[i][j];
+            }
+        }
+        alert("[The End]\n Score : " + score + "\nMax Number : " + maxNum);
+    }
+    //else createNumber();
 }
